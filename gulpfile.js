40133761fb576fa568abcd5ccbad015c9113bc8c
alienat3d <psy.alienated@gmail.com>
2021-08-26 16:19:35 +0300

    finished workflow sect.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 const { src, dest, watch, parallel, series } = require("gulp");
 
-const scss          = require('gulp-sass');
-const concat        = require('gulp-concat');
-const browserSync   = require('browser-sync').create();
-const uglify        = require("gulp-uglify");
-const autoprefixer  = require('gulp-autoprefixer');
-const imagemin      = require('gulp-imagemin');
-const del           = require('del');
+const scss = require("gulp-sass");
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const browserSync = require("browser-sync").create();
 
 function browsersync() {
   browserSync.init({
-    server : {
-      baseDir: 'app/',
-      notify: false
-    }
+    server: {
+      baseDir: "app/",
+      notify: false,
+    },
   });
 }
 
@@ -35,10 +35,6 @@ function scripts() {
   return src([
     "node_modules/jquery/dist/jquery.js",
     "node_modules/slick-carousel/slick/slick.js",
-    "node_modules/rateyo/src/jquery.rateyo.js",
-    "node_modules/@fancyapps/ui/dist/fancybox.umd.js",
-    "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
-    "node_modules/jquery-form-styler/dist/jquery.formstyler.js",
     "app/js/main.js",
   ])
     .pipe(concat("main.min.js"))
