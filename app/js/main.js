c8544754dfca2c39d5aac76a2e35fbb086bea596
alienat3d <psy.alienated@gmail.com>
2021-09-02 01:28:46 +0300

    finished media for header

--- app/js/main.js
+++ app/js/main.js
@@ -6,13 +6,20 @@ $(function () {
   $(".rightside-menu__close").on("click", function () {
     $(".rightside-menu").addClass("rightside-menu--close");
   });
+
+
+  $(".header__button-menu").on('click', function () {
+    $(".menu").toggleClass("menu--opened");
+  });
   
+
   $(".top__slider").slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true
   });
+
 
   $(".contact-slider").slick({
     slidesToShow: 10,
@@ -23,11 +30,13 @@ $(function () {
     autoplay: true
   });
 
+
   $(".article-slider__box").slick({
     arrows: true,
     prevArrow: "<button type='button' class='slick-prev'><img class='article-slider__angle-left-icon' src='images/icons/angle-left.svg' alt='angle left icon'></button>",
     nextArrow: "<button type='button' class='slick-next'><img class='article-slider__angle-right-icon' src='images/icons/angle-right.svg' alt='angle right icon'></button>"
   });
+
 
   var mixer = mixitup(".gallery__inner", {
     load: {
--- app/readme.txt
+++ app/readme.txt
@@ -3,3 +3,7 @@ TODO: check & correct all links;
 TODO: add some more images of furniture with transparent bg to top slider (index.html);
 
 FIXME: probably have to move remember me checkbox from blog-one.html to contact.html and also remove e-mail input;
+
+FIXME: SVG on position absolute is not SVG, but PNG, change it.
+
+FIXME: make .header__button-menu menu same color as Logo.
