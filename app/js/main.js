0952d1f16c16299653de4fb8290acde63434b288
alienat3d <psy.alienated@gmail.com>
2021-09-04 20:15:03 +0300

    finished adaptive for index.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,46 +1,60 @@
 $(function () {
-  
-  $(".header__button").on('click', function () {
+  // === ACTIVATE RIGHT SIDE MENU ON DESKTOP ===
+  $(".header__button").on("click", function () {
     $(".rightside-menu").removeClass("rightside-menu--close");
   });
   $(".rightside-menu__close").on("click", function () {
     $(".rightside-menu").addClass("rightside-menu--close");
   });
+  // === / ACTIVATE RIGHT SIDE MENU ON DESKTOP ===
 
-
-  $(".header__button-menu").on('click', function () {
+  // === ACTIVATE BURGER MENU ===
+  $(".header__button-menu").on("click", function () {
     $(".menu").toggleClass("menu--opened");
   });
-  
+  // === / ACTIVATE BURGER MENU ===
 
+  // === MOVING .WORKFLOW__ITEM--MEASUREMENTS INSIDE OF .WORKFLOW__ITEMS-BOX FOR ADAPTIVE ===
+  if ($(window).width() < 651) {
+    $(".workflow__item--measurements").appendTo($(".workflow__items-box"));
+  }
+  // === / MOVING .WORKFLOW__ITEM--MEASUREMENTS INSIDE OF .WORKFLOW__ITEMS-BOX FOR ADAPTIVE ===
+
+  // === TOP SLIDER IN INDEX.HTML ===
   $(".top__slider").slick({
     dots: true,
     arrows: false,
     fade: true,
-    autoplay: true
+    autoplay: true,
   });
+  // === / TOP SLIDER IN INDEX.HTML ===
 
-
+  // === SLIDER IN CONTACT.HTML ===
   $(".contact-slider").slick({
     slidesToShow: 10,
     slidesToScroll: 5,
     autoplaySpeed: 7000,
     dots: true,
     arrows: false,
-    autoplay: true
+    autoplay: true,
   });
+  // === / SLIDER IN CONTACT.HTML ===
 
-
+  // === SLIDER IN BLOG.HTML ===
   $(".article-slider__box").slick({
     arrows: true,
-    prevArrow: "<button type='button' class='slick-prev'><img class='article-slider__angle-left-icon' src='images/icons/angle-left.svg' alt='angle left icon'></button>",
-    nextArrow: "<button type='button' class='slick-next'><img class='article-slider__angle-right-icon' src='images/icons/angle-right.svg' alt='angle right icon'></button>"
+    prevArrow:
+      "<button type='button' class='slick-prev'><img class='article-slider__angle-left-icon' src='images/icons/angle-left.svg' alt='angle left icon'></button>",
+    nextArrow:
+      "<button type='button' class='slick-next'><img class='article-slider__angle-right-icon' src='images/icons/angle-right.svg' alt='angle right icon'></button>",
   });
+  // === / SLIDER IN BLOG.HTML ===
 
-
+  // === ACTIVATE MIXITUP PLUGIN IN INDEX.HTML ===
   var mixer = mixitup(".gallery__inner", {
     load: {
       filter: ".cat-living-room",
     },
   });
+  // === / ACTIVATE MIXITUP PLUGIN IN INDEX.HTML ===
 })
