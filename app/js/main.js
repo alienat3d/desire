0f5b191fb38b668676ab881adf989d4080eec88f
alienat3d <psy.alienated@gmail.com>
2021-09-04 23:48:57 +0300

    almost finished adaptive for contact.html

--- app/js/main.js
+++ app/js/main.js
@@ -37,6 +37,50 @@ $(function () {
     dots: true,
     arrows: false,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 4,
+        }
+      },
+      {
+        breakpoint: 1511,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 541,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 376,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
   });
   // === / SLIDER IN CONTACT.HTML ===
 
