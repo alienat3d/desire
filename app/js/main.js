7ef9b2250bde3548c9f3d58f7c0667d2bda9a13a
alienat3d <psy.alienated@gmail.com>
2021-09-13 13:24:07 +0300

    before I make changes to slider [contact.html]

--- app/js/main.js
+++ app/js/main.js
@@ -33,54 +33,54 @@ $(function () {
   $(".contact-slider").slick({
     slidesToShow: 10,
     slidesToScroll: 5,
-    autoplaySpeed: 7000,
+    // autoplaySpeed: 7000,
     dots: true,
     arrows: false,
-    autoplay: true,
-    responsive: [
-      {
-        breakpoint: 1700,
-        settings: {
-          slidesToShow: 8,
-          slidesToScroll: 4,
-        }
-      },
-      {
-        breakpoint: 1511,
-        settings: {
-          slidesToShow: 6,
-          slidesToScroll: 3,
-        }
-      },
-      {
-        breakpoint: 1201,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 2,
-        }
-      },
-      {
-        breakpoint: 841,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        }
-      },
-      {
-        breakpoint: 541,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        }
-      },
-      {
-        breakpoint: 376,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      },
-    ]
+    autoplay: false,
+    // responsive: [
+    //   {
+    //     breakpoint: 1700,
+    //     settings: {
+    //       slidesToShow: 8,
+    //       slidesToScroll: 4,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 1511,
+    //     settings: {
+    //       slidesToShow: 6,
+    //       slidesToScroll: 3,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 1201,
+    //     settings: {
+    //       slidesToShow: 4,
+    //       slidesToScroll: 2,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 841,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 541,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 376,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //     }
+    //   },
+    // ]
   });
   // === / SLIDER IN CONTACT.HTML ===
 
