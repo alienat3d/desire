271042a526ee811abb8fe80aaa05e8d42c5254cc
alienat3d <psy.alienated@gmail.com>
2021-09-14 18:48:56 +0300

    working vers. of slider contact.html

--- app/js/main.js
+++ app/js/main.js
@@ -52,6 +52,35 @@ $(function () {
         slidesToScroll: 3
       }
     },
+    {
+      breakpoint: 1181,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4
+      }
+    },
+    {
+      breakpoint: 841,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 551,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 376,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+    },
     // {
     //   breakpoint: 1761,
     //   settings: {
