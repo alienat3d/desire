38ac4c1b52da7bf4cd2274625e086b38b5b480ed
Al Tsaplin <al.tsaplin@gmail.com>
2024-01-07 01:01:28 +0300

    Add (favicons) Generated and include favicons

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest, series, watch} = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 const del = require('del');
 const sass = require('gulp-dart-sass');
 const squoosh = require('gulp-squoosh');
@@ -14,19 +14,19 @@ const sync = require('browser-sync').create();
 // const webpackStream = require('webpack-stream');
 const gulpHtmlBemValidator = require('gulp-html-bem-validator');
 const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require("gulp-autoprefixer");
-const fonter = require("gulp-fonter");
-const ttf2woff2 = require("gulp-ttf2woff2");
+const autoprefixer = require('gulp-autoprefixer');
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
 
 const sourceFolder = 'app'; //папка куда собираем все исходники проекта (html, scss, js, img и т.п.)
 const buildFolder = 'docs'; //папка куда собирается проект (указываем docs, если нужен gitHubPage, дополнительно нужно указать в настройках gitHub)
 
 function html() {
-  return src(sourceFolder + "/html/**/*.html")
+  return src(sourceFolder + '/html/**/*.html')
     .pipe(include())
     // .pipe(webpAvifHTML())
     .pipe(gulpHtmlBemValidator())
-    .pipe(cachebust({ type: "timestamp" }))
+    .pipe(cachebust({ type: 'timestamp' }))
     .pipe(dest(buildFolder))
 };
 
@@ -45,31 +45,31 @@ function svg() {
 function sprite() {
   return src(sourceFolder + '/img/icons/**/*.svg')
     .pipe(svgstore({ inlineSvg: true }))
-    .pipe(rename("sprite.svg"))
+    .pipe(rename('sprite.svg'))
     .pipe(dest(buildFolder + '/img/icons'))
 };
 
 function scss() {
-  return src(sourceFolder + "/scss/main.scss")
+  return src(sourceFolder + '/scss/main.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(cleanCSS({ level: 2 }))
-    .pipe(rename("main.min.css"))
-    .pipe(sass().on("error", sass.logError))
+    .pipe(rename('main.min.css'))
+    .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(dest(buildFolder + "/css"))
+    .pipe(dest(buildFolder + '/css'))
 };
 
 function js() {
   return (
-    src(sourceFolder + "/js/*.js")
+    src(sourceFolder + '/js/*.js')
       .pipe(sourcemaps.init())
       // .pipe(
       //   webpackStream({
-      //     mode: "none",
+      //     mode: 'none',
       //     output: {
-      //       filename: "main.min.js",
+      //       filename: 'main.min.js',
       //     },
       //     module: {
       //       rules: [
@@ -77,9 +77,9 @@ function js() {
       //           test: /\.m?js$/,
       //           exclude: /node_modules/,
       //           use: {
-      //             loader: "babel-loader",
+      //             loader: 'babel-loader',
       //             options: {
-      //               presets: [["@babel/preset-env", { targets: "defaults" }]],
+      //               presets: [['@babel/preset-env', { targets: 'defaults' }]],
       //             },
       //           },
       //         },
@@ -89,13 +89,13 @@ function js() {
       // )
       .pipe(uglify())
       .pipe(sourcemaps.write())
-      .pipe(dest(buildFolder + "/js"))
+      .pipe(dest(buildFolder + '/js'))
   );
 };
 
 function img() {
   return src(sourceFolder + '/img/**/*.{png,jpg}')
-    .pipe(dest(buildFolder + "/img"))
+    .pipe(dest(buildFolder + '/img'))
     .pipe(
       squoosh(() => ({
         encodeOptions: {
@@ -104,19 +104,19 @@ function img() {
         },
       }))
     )
-    .pipe(dest(buildFolder + "/img"));
+    .pipe(dest(buildFolder + '/img'))
 };
 
 function convertFonts() {
-  return src(sourceFolder + "/fonts/src/*.*")
-    .pipe(fonter({ formats: [ "woff", "ttf" ] }))
-    .pipe(src(sourceFolder + "/fonts/*.ttf"))
+  return src(sourceFolder + '/fonts/src/*.*')
+    .pipe(fonter({ formats: ['woff', 'ttf'] }))
+    .pipe(src(sourceFolder + '/fonts/*.ttf'))
     .pipe(ttf2woff2())
-    .pipe(dest(sourceFolder + "/fonts"));
+    .pipe(dest(sourceFolder + '/fonts'))
 }
 
 function fonts() {
-  return src(sourceFolder + "/fonts/*.*").pipe(dest(buildFolder + "/fonts"));
+  return src(sourceFolder + '/fonts/*.*').pipe(dest(buildFolder + '/fonts'))
 };
 
 function clear() {
@@ -124,8 +124,12 @@ function clear() {
 };
 
 function clearBlocksDir() {
-  return del(buildFolder + "/blocks")
+  return del(buildFolder + '/blocks')
 };
+
+function copy() {
+  return src(sourceFolder + '/favicons/*.*').pipe(dest(buildFolder + '/favicons'))
+}
 
 function serve() {
   sync.init({
@@ -133,9 +137,9 @@ function serve() {
     reloadOnRestart: true,
     server: {
       baseDir: buildFolder,
-      directory: true, // чтобы загружался сразу index.html поменять на "false"
+      directory: true, // чтобы загружался сразу index.html поменять на 'false'
     },
-    notify: false, // чтобы всплывало сообщение об обновлении браузера поменять на "true"
+    notify: false, // чтобы всплывало сообщение об обновлении браузера поменять на 'true'
   });
 
   watch(sourceFolder + '/html/**/*.html', series(html)).on('change', sync.reload)
@@ -145,11 +149,12 @@ function serve() {
   watch(sourceFolder + '/img/**/*', series(svg)).on('change', sync.reload)
   watch(sourceFolder + '/img/icons/**/*', series(sprite)).on('change', sync.reload)
   watch(sourceFolder + '/fonts/**/*', series(fonts)).on('change', sync.reload)
+  watch(sourceFolder + '/favicons/**/*', series(copy)).on('change', sync.reload)
 };
 
 
 exports.build = series(clear, scss, js, img, sprite, fonts, html, clearBlocksDir);
-exports.default = series(clear, scss, js, img, svg, sprite, fonts, html, serve);
+exports.default = series(clear, scss, js, img, svg, sprite, fonts, html, copy, clearBlocksDir, serve);
 exports.fonts = convertFonts;
 exports.bem = bem;
 exports.clear = clear;
